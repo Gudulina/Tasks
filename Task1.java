5b0536f56399830b3a869a9a09e5a2e0dc6312d1
Elena <gudulina@mail.ru>
2024-03-03 18:29:16 +0700

    Initial commit

---
+++ Task1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int x = input.nextInt();
+        System.out.print("Введите второе число: ");
+        int y = input.nextInt();
+        int sum = x+y;
+        System.out.println(x +" + "+y+" = "+ sum);
+    }
+}
